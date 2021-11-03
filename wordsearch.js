2f551506f9cc13b38dee6c97407ca238a6a8e4e9
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-03 14:08:17 -0400

    pair programming with Abdullah

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    // console.log(horizontalJoin);
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    const flipped = helper(letters);
+    const flippedJoin = flipped.map(ls => ls.join(''));
+    // console.log(flippedJoin);
+    for (l of flippedJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+  
+  }
+  
+  module.exports = wordSearch
+  
+  const helper = function(matrix) {
+    let newArray = [];
+    
+    //this will create the required rows for our empty array.
+    for (let i = 0; i < matrix[0].length; i++) {
+        newArray.push([]);
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            newArray[j].push(matrix[i][j]);
+        }
+    }
+    return newArray;
+  };
+  
